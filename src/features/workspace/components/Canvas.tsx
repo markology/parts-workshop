9f6bc8e0114a1088b10a6166d2a6e285ba1b0c56
MSI\User <markjacobacosta@gmail.com>
2025-04-24 01:17:55 -0500

    mobil view only

--- src/features/workspace/components/Canvas.tsx
+++ src/features/workspace/components/Canvas.tsx
@@ -11,9 +11,12 @@ import {
 } from "@xyflow/react";
 import Utilities from "./Utilities/Utilities";
 import { useJournalStore } from "@/state/Journal";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 const Workspace = () => {
   const isOpen = useJournalStore((s) => s.isOpen);
+  const isMobile = useIsMobile();
+
   const {
     edges,
     handleNodeDragStop,
@@ -28,27 +31,49 @@ const Workspace = () => {
 
   return (
     <div id="canvas" className="reactflow-wrapper">
-      <ReactFlow
-        className="h-[4000px] w-[4000px]"
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange as OnNodesChange<Node>}
-        onEdgesChange={onEdgeChange}
-        onNodeDragStop={handleNodeDragStop}
-        onConnect={onConnect}
-        onDrop={onDrop}
-        onDragOver={onDragOver}
-        fitView
-        onPaneClick={handlePaneClick}
-        nodeTypes={nodeTypes}
-        defaultViewport={{ x: 0, y: 0, zoom: 1 }}
-        minZoom={0}
-        deleteKeyCode="Delete"
-        maxZoom={2}
-      >
-        <Background />
-        <Controls className="absolute bottom-4 left-4" />
-      </ReactFlow>
+      {!isMobile ? (
+        <ReactFlow
+          className="h-[4000px] w-[4000px]"
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange as OnNodesChange<Node>}
+          onEdgesChange={onEdgeChange}
+          onNodeDragStop={handleNodeDragStop}
+          onConnect={onConnect}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          fitView
+          onPaneClick={handlePaneClick}
+          nodeTypes={nodeTypes}
+          defaultViewport={{ x: 0, y: 0, zoom: 1 }}
+          minZoom={0}
+          deleteKeyCode="Delete"
+          maxZoom={2}
+        >
+          <Background />
+          <Controls className="absolute bottom-4 left-4" /> :
+        </ReactFlow>
+      ) : (
+        <ReactFlow
+          className="h-[4000px] w-[4000px]"
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={() => {}}
+          onEdgesChange={() => {}}
+          onConnect={() => {}}
+          nodesDraggable={false}
+          nodesConnectable={false}
+          elementsSelectable={false}
+          nodeTypes={nodeTypes}
+          fitView
+          defaultViewport={{ x: 0, y: 0, zoom: 1 }}
+          minZoom={0}
+          maxZoom={2}
+        >
+          <Background />
+          <Controls className="absolute bottom-4 left-4" />
+        </ReactFlow>
+      )}
       {!isOpen && <Utilities full />} {/* now Zustand-powered */}
     </div>
   );
