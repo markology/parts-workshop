6740f561edd3e9a7f66cc406222e76eac8a9d458
MSI\User <markjacobacosta@gmail.com>
2025-05-02 16:35:43 -0500

    remove unused files and ts

--- src/components/ViewportSizeSwitch.tsx
+++ src/components/ViewportSizeSwitch.tsx
@@ -2,7 +2,13 @@
 
 import { useIsMobile } from "@/hooks/useIsMobile";
 
-export default function ViewportSizeSwitch({ mobile, desktop }) {
+export default function ViewportSizeSwitch({
+  mobile,
+  desktop,
+}: {
+  mobile: React.ReactNode;
+  desktop: React.ReactNode;
+}) {
   const isMobile = useIsMobile();
 
   if (isMobile === null) return null;
