b2f3c7a63c77c8ff4d40371b65c274b25f6f9dcd
MSI\User <markjacobacosta@gmail.com>
2026-01-01 02:32:03 -0600

    remove unused tailwind plugins and add safelist colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
   darkMode: "class",
+  // Safelist allows these classes to always be generated, even if not found in source code
+  // Useful for testing classes via browser console
+  safelist: [
+    // Theme-dark variants with common colors for testing
+    'theme-dark:bg-black',
+    'theme-dark:bg-white',
+    'theme-dark:bg-red-500',
+    'theme-dark:bg-blue-500',
+    'theme-dark:bg-green-500',
+    // Add any other classes you want to test via console
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    plugin(function ({ addVariant }) {
-      addVariant("theme-cherry", ".theme-cherry &");
-      addVariant("theme-dark", ".theme-dark &");
-      addVariant("theme-light", ".theme-light &");
-    }),
-  ],
+  plugins: [],
 };
